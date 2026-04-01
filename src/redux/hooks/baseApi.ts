89fd564bb6a06b488ee6e4e1b692f7889d425899
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-04-01 09:24:23 +0600

    d

--- src/redux/hooks/baseApi.ts
+++ src/redux/hooks/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import Cookies from "js-cookie";
 
 // const baseURL = import.meta.env.VITE_API_ENDPOINT;
-const baseURL = `https://api.highlightzapp.co/api/v1`;
+const baseURL = `https://api.highlightzapp.com/api/v1`;
 // const baseURL = "http://localhost:5001/api/v1";
 if (!baseURL) {
   throw new Error("VITE_API_ENDPOINT is not defined in environment variables");
